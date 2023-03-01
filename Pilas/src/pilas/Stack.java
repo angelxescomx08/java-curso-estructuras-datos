f6dd80ce80cc238685167c896053cb6de9cec289
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-02-28 20:39:53 -0600

    Pila

---
+++ Pilas/src/pilas/Stack.java
@@ -0,0 +1,33 @@
+package pilas;
+
+public class Stack<T extends Comparable<T>> {
+	private Node<T> head;
+	private int count;
+	
+	public void push(T data) {
+		count++;
+		
+		if(head == null) {
+			head = new Node<>(data);
+		}else {
+			Node<T> oldHead = head;
+			head = new Node<>(data);
+			head.setNextNode(oldHead);
+		}
+	}
+	
+	public T pop() {
+		T item = head.getData();
+		head = head.getNextNode();
+		count--;
+		return item;
+	}
+	
+	public int size() {
+		return count;
+	}
+	
+	public boolean isEmpty() {
+		return count == 0;
+	}
+}
