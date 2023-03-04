678eb819e933185f65edbf756ca2337fd827258f
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-03 19:41:40 -0600

    correccion de warning

--- Colas/src/colas/App.java
+++ Colas/src/colas/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Cola<Integer> myQueue = new Cola();
+		Cola<Integer> myQueue = new Cola<>();
 		myQueue.enqueue(10);
 		myQueue.enqueue(100);
 		myQueue.enqueue(1000);
