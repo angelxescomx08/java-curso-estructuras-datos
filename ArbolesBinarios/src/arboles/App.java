576ee566d39bc494118fdb658cf7ceeea256eb62
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-10 21:03:04 -0600

    insertar

--- ArbolesBinarios/src/arboles/App.java
+++ ArbolesBinarios/src/arboles/App.java
@@ -2,4 +2,16 @@ package arboles;
 
 public class App {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArbolBinario<Integer> ab = new ArbolBinario<>();
+		ab.insert(10);
+		ab.insert(5);
+		ab.insert(15);
+		ab.insert(14);
+		ab.insert(16);
+		ab.insert(-1);
+		
+	}
+
 }
--- ArbolesBinarios/src/arboles/ArbolBinario.java
+++ ArbolesBinarios/src/arboles/ArbolBinario.java
@@ -1,5 +1,65 @@
 package arboles;
 
-public class ArbolBinario {
+public class ArbolBinario<T extends Comparable<T>> implements Tree<T>{
+
+	private Node<T> root;
+	
+	@Override
+	public void insert(T data) {
+		// TODO Auto-generated method stub
+		if(root == null) {
+			root = new Node<>(data,null);
+		}else {
+			//hay items en el arbol
+			insert(data, root);
+		}
+	}
+
+	private void insert(T data, Node<T> node) {
+		// TODO Auto-generated method stub
+		//la data es más pequeña
+		if(node.getData().compareTo(data) < 0) {
+			//sigue buscadando en el subarbol izquierdo
+			if(node.getLeftChild() != null)
+				insert(data, node.getLeftChild());
+			else
+				//setear el nodo izquierdo
+				node.setLeftChild(new Node<>(data,node));
+		//mayor
+		}else {
+			if(node.getData().compareTo(data) < 0) {
+				//sigue buscadando en el subarbol derecho
+				if(node.getRightChild() != null)
+					insert(data, node.getRightChild());
+				else
+					//setear el nodo izquierdo
+					node.setRightChild(new Node<>(data,node));
+			}
+		}
+	}
+
+	@Override
+	public void remove(T data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void traversal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T getMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
