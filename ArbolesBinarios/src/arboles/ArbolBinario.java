abca7a6ab43b1092e3097884bfb807caf5ceac7e
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-11 14:53:46 -0600

    remover item de un arbol binario

--- ArbolesBinarios/src/arboles/ArbolBinario.java
+++ ArbolesBinarios/src/arboles/ArbolBinario.java
@@ -40,7 +40,100 @@ public class ArbolBinario<T extends Comparable<T>> implements Tree<T>{
 
 	@Override
 	public void remove(T data) {
-		// TODO Auto-generated method stub
+		if( root != null)
+			remove(data, root);
+	}
+
+	private void remove(T data, Node<T> node) {
+		
+		if(node == null)return;
+		
+		//buscamos el item que queremos remover
+		if(data.compareTo(node.getData()) < 0) {
+			remove(data,node.getLeftChild());
+		}else if(data.compareTo(node.getData()) > 0) {
+			remove(data,node.getRightChild());
+		}else {
+			//Es el item que se quiere remover
+			
+			
+			//si el nodo es una hoja
+			if(node.getLeftChild() == null && node.getRightChild() == null) {
+				//checar si es hijo izquierdo o derecho
+				Node<T> parent = node.getParentNode();
+				
+				//parent != null para ver si es la raíz
+				if(parent != null && parent.getLeftChild() == node) {
+					parent.setLeftChild(null);
+				}else if(parent != null && parent.getRightChild() == node) {
+					parent.setRightChild(null);
+				}
+				
+				//si la raiz es el nodo que se quiere remover
+				if(parent == null)
+					root = null;
+				
+				//remueve el nodo y hace que lo pueda eliminar el garbage colector
+				node = null;
+			}
+			//remover items con un hijo
+			else if(node.getLeftChild() == null && node.getRightChild() != null) {
+				Node<T> parent = node.getParentNode();
+				
+				//parent != null para ver si es la raíz
+				if(parent != null && parent.getLeftChild() == node) {
+					parent.setLeftChild(node.getRightChild());
+				}else if(parent != null && parent.getRightChild() == node) {
+					parent.setRightChild(node.getRightChild());
+				}
+				
+				if(parent == null) {
+					root =  node.getRightChild();
+				}
+				
+				node.getRightChild().setParentNode(parent);
+				node = null;
+			}
+			
+			//remover items con un hijo (left)
+			else if(node.getLeftChild() != null && node.getRightChild() == null) {
+				Node<T> parent = node.getParentNode();
+				
+				//parent != null para ver si es la raíz
+				if(parent != null && parent.getLeftChild() == node) {
+					parent.setLeftChild(node.getLeftChild());
+				}else if(parent != null && parent.getRightChild() == node) {
+					parent.setRightChild(node.getRightChild());
+				}
+				
+				if(parent == null) {
+					root =  node.getLeftChild();
+				}
+				
+				node.getLeftChild().setParentNode(parent);
+				node = null;
+			}
+			//remover uno que tiene dos hijos
+			else {
+				//find el predecesor
+				Node<T> predecesor = getPredecessor(node.getLeftChild());
+				
+				//intercambiamos los valores
+				T tmp = predecesor.getData();
+				predecesor.setData(node.getData());
+				node.setData(tmp);
+				
+				//llamanos a remover con el predecesor
+				remove(data, predecesor);
+			}
+		}
+	}
+
+	private Node<T> getPredecessor(Node<T> node) {
+		if (node.getRightChild() != null)
+			return getPredecessor(node.getRightChild());
+		
+		return node;
 		
 	}
 
